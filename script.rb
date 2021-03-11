aa37161f0a1f8e961f3074a4a7afef760d450dd7
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-03-11 12:49:33 +0900

    update logic/test

--- script.rb
+++ script.rb
@@ -5,3 +5,17 @@ class Sample
     42
   end
 end
+
+class BendingMachine
+  def initialize
+    @totalAmount = 0
+  end
+  def input(money)
+    if [10, 50, 100, 500, 1000].include?(money)
+      @totalAmount += money
+    end
+  end
+  def dispAmount
+    return @totalAmount
+  end
+end
--- test.rb
+++ test.rb
@@ -3,17 +3,23 @@
 require_relative 'script'
 
 RSpec.describe do
-  describe 'Sample' do
-    it { expect(Sample.perfect_answer).to eq 42 }
-  end
+  # describe 'Sample' do
+  #  it { expect(Sample.perfect_answer).to eq 42 }
+  # end
   # 硬貨の種類？
   describe 'お金の投入' do
     describe '想定内のお金' do
       it '投入金額がhogehogeになる' do
+        machine = BendingMachine.new
+        machine.input(500)
+        expect(machine.dispAmount).to eq 500
       end
     end
     describe '想定外のお金' do
       it '投入金額が変わらない' do
+        machine = BendingMachine.new
+        machine.input(1)
+        expect(machine.dispAmount).to eq 0
       end
     end
   end
