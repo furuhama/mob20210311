b00b6bf108673e62f36c998b87790722a8b9bec1
n4r1tahhhh <narita.ryutaro@moneyforward.co.jp>
2021-03-11 12:32:30 +0900

    1回目

--- test.rb
+++ test.rb
@@ -3,7 +3,18 @@
 require_relative 'script'
 
 RSpec.describe do
-  describe 'Sample' do
-    it { expect(Sample.perfect_answer).to eq 42 }
+  
+  describe 'Vending Machine' do
+    let(:machine) { VendingMachine.new() }
+    let(:str) { 'hoge' }
+    context '想定内のお金' do
+      let(:str) { '1000' }
+      it 'input' do
+
+        expect{machine.input(str)}.to change{
+
+        }
+      end
+    end
   end
 end
