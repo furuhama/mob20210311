73e2032d3f6dfc75a4158bf8fec0864a36b9f9a3
Naoto Ono <nono19@students.desu.edu>
2021-03-11 12:34:04 +0900

    hoge

--- test.rb
+++ test.rb
@@ -6,4 +6,23 @@ RSpec.describe do
   describe 'Sample' do
     it { expect(Sample.perfect_answer).to eq 42 }
   end
+  # 硬貨の種類？
+  describe 'お金の投入' do
+    describe '想定内のお金' do
+      it '投入金額がhogehogeになる' do
+      end
+    end
+    describe '想定外のお金' do
+      it '投入金額が変わらない' do
+      end
+    end
+  end
+  # 払い戻しの時は払戻という文字列が入力されるのか？ -> 0とする?
+  # 
+  describe '払い戻し' do
+    it '投入金額が0になる' do
+    end
+    it '釣り銭を出力' do
+    end
+  end
 end
