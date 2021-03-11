d963bbb57dc242cc73c58eaccd7b1b3722127cb5
harada-ikuhiro <harada.ikuhiro@moneyforward.co.jp>
2021-03-11 13:05:44 +0900

    リファクタ

--- test.rb
+++ test.rb
@@ -8,43 +8,42 @@ RSpec.describe do
   # end
   # 硬貨の種類？
   describe 'お金の投入' do
+    let(:machine) {BendingMachine.new}
+
     describe '想定内のお金' do
       it '投入金額がhogehogeになる' do
-        machine = BendingMachine.new
         machine.input(500)
         expect(machine.dispAmount).to eq 500
       end
     end
+
+    it '想定内のお金（複数回の場合）' do
+      machine.input(10)
+      machine.input(50)
+      machine.input(100)
+      machine.input(500)
+      machine.input(1000)
+      expect(machine.dispAmount).to eq 1660
+    end
+
     describe '想定外のお金' do
       it '投入金額が変わらない' do
-        machine = BendingMachine.new
         machine.input(1)
         expect(machine.dispAmount).to eq 0
-      end
-    end
-    describe '想定内のお金（複数回の場合）' do
-      it do
-        machine = BendingMachine.new
-        machine.input(10)
-        machine.input(50)
-        machine.input(100)
-        machine.input(500)
-        machine.input(1000)
-        expect(machine.dispAmount).to eq 1660
       end
     end
   end
   # 払い戻しの時は払戻という文字列が入力されるのか？ -> 0とする?
   # 
   describe '払い戻し' do
+    let(:machine) {BendingMachine.new}
+
     it '投入金額が0になる' do
-      machine = BendingMachine.new
       machine.input(500)
       machine.output
       expect(machine.dispAmount).to eq 0
     end
     it '釣り銭を出力' do
-      machine = BendingMachine.new
       machine.input(500)
       expect(machine.output).to eq 500
     end
