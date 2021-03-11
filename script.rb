556a312f7a34a2005c2fc111d9d2987c3271dd4b
thinceller <thinceller@gmail.com>
2021-03-11 12:50:52 +0900

    2回目

--- script.rb
+++ script.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
-class Sample
-  def self.perfect_answer
-    42
+class VendingMachine
+  attr_accessor :sum_value
+
+  def initialize
+    self.sum_value = 0
+  end
+
+  def input(str)
+    self.sum_value += str.to_i
+    nil
   end
 end
--- test.rb
+++ test.rb
@@ -10,10 +10,26 @@ RSpec.describe do
     context '想定内のお金' do
       let(:str) { '1000' }
       it 'input' do
-
         expect{machine.input(str)}.to change{
+          machine.sum_value
+        }.by(1000)
+      end
 
-        }
+      it 'return value' do
+        expect(machine.input(str)).to eq nil
+      end
+    end
+
+    context '想定外のお金' do
+      let(:str) { '1' }
+      it 'input' do
+        expect{machine.input(str)}.not_to change{
+          machine.sum_value
+        }.by(1000)
+      end
+
+      it 'return value' do
+        expect(machine.input(str)).to eq nil
       end
     end
   end
